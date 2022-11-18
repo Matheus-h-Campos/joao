abe869899eb73cda1425413ff23d0f53697a1ae3
Matheus Campos <camposs.matheus@gmail.com>
2022-11-18 15:16:10 -0300

    home testimonials

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
+import Section_6 from '../components/lp_components/Section_6';
 import Section_1 from '../components/Section_1';
 import Section_2 from '../components/Section_2';
 import Section_3 from '../components/Section_3';
@@ -20,6 +21,7 @@ export default function Home() {
         <Section_2 />
         <Section_3 />
         <Section_4 />
+        <Section_6 />
         <Footer />
       </main>
     </>
