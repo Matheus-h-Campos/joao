0773c612accc40f1494c9600972570275577b460
Matheus Campos <camposs.matheus@gmail.com>
2022-11-18 19:09:01 -0300

    url lp update

--- pages/zero-ao-pull-up.tsx
+++ pages/zero-ao-pull-up.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
-import Hero from '../../components/lp_components/Hero';
-import Section_1 from '../../components/lp_components/Section_1';
-import Section_2 from '../../components/lp_components/Section_2';
-import Section_3 from '../../components/lp_components/Section_3';
-import Section_4 from '../../components/lp_components/Section_4';
-import Section_5 from '../../components/lp_components/Section_5';
-import Section_6 from '../../components/lp_components/Section_6';
+import Hero from '../components/lp_components/Hero';
+import Section_1 from '../components/lp_components/Section_1';
+import Section_2 from '../components/lp_components/Section_2';
+import Section_3 from '../components/lp_components/Section_3';
+import Section_4 from '../components/lp_components/Section_4';
+import Section_5 from '../components/lp_components/Section_5';
+import Section_6 from '../components/lp_components/Section_6';
 
 type Props = {};
 
