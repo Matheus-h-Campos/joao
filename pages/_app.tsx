ed12605d20651ed1676ec4bf6dd660f2309c7cda
Matheus Campos <camposs.matheus@gmail.com>
2022-11-19 10:43:51 -0300

    fb domain verification 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Coach João André</title>
+        <meta
+          name="facebook-domain-verification"
+          content="y40iu433capigubaeg6yvq4deubrlw"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
