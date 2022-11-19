67e22d776d0c237360f247544b1cf7be2347d64d
Matheus Campos <camposs.matheus@gmail.com>
2022-11-19 09:25:44 -0300

    pixel installation try 2

--- components/Pixel/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Head from 'next/head';
-import FACEBOOK_PIXEL_1 from './facebook/FacebookPixel';
-
-type Props = {};
-
-export default function index({ name }: any) {
-  return <Head>{name === 'FACEBOOK_PIXEL_1' && <FACEBOOK_PIXEL_1 />}</Head>;
-}
