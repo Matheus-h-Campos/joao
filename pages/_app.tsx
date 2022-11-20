95a984957b48995717137a173ce6566838c849f7
Matheus Campos <camposs.matheus@gmail.com>
2022-11-20 13:16:34 -0300

    gtm changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,40 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import CookieConsent from 'react-cookie-consent';
 import { FacebookPixel } from '../utils/facebookPixel';
-import Script from 'next/script';
-import TagManager from '../utils/TagManager';
+import TagManager, { TagManagerArgs } from 'react-gtm-module';
+import { useEffect } from 'react';
+// import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <>
+      {/* <Script id="gtm" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WZLGR5K');
+        `}
+      </Script>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WZLGR5K"
+          height="0"
+          width="0"
+          className="hidden"
+        ></iframe>
+      </noscript> */}
       <Head>
-        <TagManager />
         <title>Professor João André</title>
         <meta
           name="facebook-domain-verification"
