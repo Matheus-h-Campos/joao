66ad88fa8e98ac2a8d24c190465e2225ba6b420d
Matheus Campos <camposs.matheus@gmail.com>
2022-12-11 21:12:10 -0300

    facebook pixel erase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script';
-import { FacebookPixel } from '../utils/facebookPixel';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
-
   return (
     <>
-      <Script id="gtm" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${gtmId}');
-        `}
-      </Script>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-WZLGR5K"
-          height="0"
-          width="0"
-          className="hidden"
-        ></iframe>
-      </noscript>
-      <FacebookPixel />
       <Head>
         <title>Professor João André</title>
-        <meta
-          name="facebook-domain-verification"
-          content="y40iu433capigubaeg6yvq4deubrlw"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Component {...pageProps} />
     </>
   );
